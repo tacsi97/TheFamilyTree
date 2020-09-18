66cd740ace44cc010a34c0e526221dedb2dc67f1
tacsi97 <tacsi97@gmail.com>
2020-09-18 16:58:07 +0200

    relationship module

---
+++ FamilyTree.Modules.Relationship/RelationshipModule.cs
@@ -0,0 +1,20 @@
+﻿using FamilyTree.Modules.Relationship.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace FamilyTree.Modules.Relationship
+{
+    public class RelationshipModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
