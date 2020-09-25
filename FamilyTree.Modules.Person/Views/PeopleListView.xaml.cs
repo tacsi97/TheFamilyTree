f7a4cf0c798ed2597067c6a36cebaeea2ebb3cd8
tacsi97 <tacsi97@gmail.com>
2020-09-25 17:05:44 +0200

    Visualize people in a listview with dependentview injection

--- FamilyTree.Modules.Person/Views/PeopleListView.xaml.cs
+++ FamilyTree.Modules.Person/Views/PeopleListView.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System.Windows.Controls;
+﻿using FamilyTree.Core;
+using FamilyTree.Core.Attributes;
+using System.Windows.Controls;
 
 namespace FamilyTree.Modules.Person.Views
 {
     /// <summary>
     /// Interaction logic for PeopleList
     /// </summary>
+    [DependentView(RegionNames.FunctionRegion, typeof(PersonFunctions))]
     public partial class PeopleListView : UserControl
     {
         public PeopleListView()
