10c28b8e21c220c9754ed5f1a9640b98e568b0ff
tacsi97 <tacsi97@gmail.com>
2020-09-28 19:16:25 +0200

    Menupoint got deleted

--- FamilyTree.Modules.Main/ViewModels/NavigationMenuViewModel.cs
+++ FamilyTree.Modules.Main/ViewModels/NavigationMenuViewModel.cs
@@ -51,7 +51,6 @@ namespace FamilyTree.Modules.Main.ViewModels
             //The navigation path should match with the .xaml name
             MenuItems.Add(new NavigationItem() { Caption = "Főoldal", NavigationPath = "MainPage" });
             MenuItems.Add(new NavigationItem() { Caption = "Családfák", NavigationPath = "FamilyTreeListView" });
-            MenuItems.Add(new NavigationItem() { Caption = "Emberek", NavigationPath = "PeopleListView" });
             MenuItems.Add(new NavigationItem() { Caption = "Kijelentkezés", NavigationPath = "Logout" });
         }
 
