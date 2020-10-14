d5d99f1edd8856761024d85b23d9d506fa1294a4
tacsi97 <tacsi97@gmail.com>
2020-10-15 00:04:21 +0200

    New dialognames added

--- FamilyTree.Modules.Person/Core/PersonDialogNames.cs
+++ FamilyTree.Modules.Person/Core/PersonDialogNames.cs
@@ -7,5 +7,11 @@ namespace FamilyTree.Modules.Person.Core
     public class PersonDialogNames
     {
         public static string AddNewPersonDialog => "AddNewPersonDialog";
+
+        public static string ModifyPersonDialog => "ModifyPersonDialog";
+
+        public static string ShowPersonDialog => "ShowPersonDialog";
+
+        public static string DeletePersonDialog => "DeletePersonDialog";
     }
 }
