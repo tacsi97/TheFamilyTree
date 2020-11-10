3f455344631daea48ed28c5cc0a844102ef18391
tacsi97 <tacsi97@gmail.com>
2020-11-10 07:59:26 +0100

    Utils

---
+++ FamilyTree.Modules.FamilyTree/Utils/IFamilyTreeMember.cs
@@ -0,0 +1,21 @@
+﻿using FamilyTree.Business;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyTree.Modules.FamilyTree.Utils
+{
+    public interface IFamilyTreeMember
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfDeath { get; set; }
+        public GenderType Gender { get; set; }
+        public IFamilyTreeMember Father { get; set; }
+        public IFamilyTreeMember Mother { get; set; }
+        public ICollection<IFamilyTreeMember> Children { get; set; }
+        public ICollection<IFamilyTreeMember> Partners { get; set; }
+    }
+}
---
+++ FamilyTree.Modules.FamilyTree/Utils/IFamilyTreeRelationship.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyTree.Modules.FamilyTree.Utils
+{
+    interface IFamilyTreeRelationship
+    {
+        public int ID { get; set; }
+        public IFamilyTreeMember Partner { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        // TODO: Adapterekkel kell kivenni minden olyan hivatkozást, ami a másik modulra vonatkozik
+        // TODO: Adaptereket megnézni a PRISM Outlook-ban
+    }
+}
