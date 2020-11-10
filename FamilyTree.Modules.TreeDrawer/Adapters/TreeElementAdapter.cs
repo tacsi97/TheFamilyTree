d189a6e6ba3cf8aca3b64343b0555a3fe7f70efb
tacsi97 <tacsi97@gmail.com>
2020-11-10 08:16:52 +0100

    TreeElementAdapter

---
+++ FamilyTree.Modules.TreeDrawer/Adapters/TreeElementAdapter.cs
@@ -0,0 +1,25 @@
+﻿using FamilyTree.Modules.TreeDrawer.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyTree.Modules.TreeDrawer.Adapters
+{
+    public abstract class TreeElementAdapter<T> : ITreeElement
+    {
+        public T TObject { get; set; }
+
+        public abstract double LeftCoordinate { get; set; }
+
+        public abstract double TopCoordinate { get; set; }
+
+        public abstract double RigthCoordinate { get; set; }
+
+        public abstract double BottomCoordinate { get; set; }
+
+        public TreeElementAdapter(T tObject)
+        {
+            TObject = tObject;
+        }
+    }
+}
