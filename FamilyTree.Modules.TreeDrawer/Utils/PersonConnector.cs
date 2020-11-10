ac0678b9ef345a7d7fd224bb40babe64f41cdf6a
tacsi97 <tacsi97@gmail.com>
2020-11-10 08:18:49 +0100

    Create Person objects from RelationShips

---
+++ FamilyTree.Modules.TreeDrawer/Utils/PersonConnector.cs
@@ -0,0 +1,75 @@
+﻿using FamilyTree.Business;
+using FamilyTree.Modules.Relationship.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyTree.Modules.TreeDrawer.Utils
+{
+    public class PersonConnector
+    {
+        public PersonConnector()
+        {
+
+        }
+
+        public ICollection<Business.Person> ConnectPeople(IEnumerable<Business.Relationship> relationships)
+        {
+            // Összes relation-t lekérdezni, ami az adott fában van. Egy relation tartalmaz 2 embert, és a köztük lévő kapcsolatot is, ezekből pedig fel lehet építeni a fát.
+
+            var result = new List<Business.Person>();
+
+            foreach (var relation in relationships)
+            {
+                if (relation.RelationType.Equals(TypeNames.Parent))
+                {
+                    // connect parents
+                    if (relation.PersonFrom.Gender.Equals(GenderType.Male))
+                        relation.PersonFrom.Father = relation.PersonTo;
+                    else
+                        relation.PersonFrom.Mother = relation.PersonTo;
+
+                    relation.PersonTo.Children.Add(relation.PersonFrom);
+                }
+                else if (relation.RelationType.Equals(TypeNames.Child))
+                {
+                    // connect child
+                    relation.PersonFrom.Children.Add(relation.PersonTo);
+
+                    if (relation.PersonFrom.Gender.Equals(GenderType.Male))
+                        relation.PersonFrom.Father = relation.PersonTo;
+                    else
+                        relation.PersonFrom.Mother = relation.PersonTo;
+                }
+                else if (relation.RelationType.Equals(TypeNames.Partner))
+                {
+                    // nem baj, ha a szülők és a gyerekek nem Relationship-ként vannak tárolva,
+                    // mivel feltöltéskor Relationshipként megy fel,
+                    // letöltéskor pedig szintén Relationshipként 
+                    // módosításkor pedig lekérjük a Relationship objektumot
+                    // connect partner
+                    relation.PersonFrom.Partners.Add(new Business.Relationship()
+                    {
+                        RelationType = relation.RelationType,
+                        From = relation.From,
+                        To = relation.To,
+                        PersonFrom = relation.PersonFrom,
+                        PersonTo = relation.PersonTo
+                    });
+
+                    relation.PersonTo.Partners.Add(new Business.Relationship()
+                    {
+                        RelationType = relation.RelationType,
+                        From = relation.From,
+                        To = relation.To,
+                        PersonFrom = relation.PersonTo,
+                        PersonTo = relation.PersonFrom
+                    });
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
