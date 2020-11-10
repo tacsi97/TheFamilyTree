31e4a331d0ec6fe8e91a5462cc74f9be3e5da54a
tacsi97 <tacsi97@gmail.com>
2020-11-10 08:11:45 +0100

    Reference instead of hardcode

---
+++ FamilyTree.Modules.Relationship/Core/DialogNames.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyTree.Modules.Relationship.Core
+{
+    public class DialogNames
+    {
+        public const string EditRelationshipDialog = "EditRelationshipView";
+        public const string DeleteRelationshipDialog = "DeleteRelationshipView";
+    }
+}
---
+++ FamilyTree.Modules.Relationship/Core/TypeNames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyTree.Modules.Relationship.Core
+{
+    public class TypeNames
+    {
+        public const string Parent = "Parent";
+        public const string Partner = "Partner";
+        public const string Child = "Child";
+    }
+}
