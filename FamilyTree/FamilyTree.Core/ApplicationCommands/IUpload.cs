1244114418ec4953747cb9bf5b155fdc03d1e28f
tacsi97 <tacsi97@gmail.com>
2020-11-10 07:48:29 +0100

    This will deleted in a refactor

---
+++ FamilyTree/FamilyTree.Core/ApplicationCommands/IUpload.cs
@@ -0,0 +1,12 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyTree.Core.ApplicationCommands
+{
+    public interface IUpload
+    {
+        CompositeCommand CompositeCommand { get; }
+    }
+}
---
+++ FamilyTree/FamilyTree.Core/ApplicationCommands/UploadNewPersonCommand.cs
@@ -0,0 +1,12 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyTree.Core.ApplicationCommands
+{
+    public class UploadNewPersonCommand : IUpload
+    {
+        public CompositeCommand CompositeCommand { get; } = new CompositeCommand();
+    }
+}
