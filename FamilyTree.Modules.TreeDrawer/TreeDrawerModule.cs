c0a6336f72b52ef563f9c45393125ec30fcac4b8
tacsi97 <tacsi97@gmail.com>
2020-11-10 08:20:23 +0100

    Some baic configuration

---
+++ FamilyTree.Modules.TreeDrawer/TreeDrawerModule.cs
@@ -0,0 +1,22 @@
+﻿using FamilyTree.Modules.TreeDrawer.ViewModels;
+using FamilyTree.Modules.TreeDrawer.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace FamilyTree.Modules.TreeDrawer
+{
+    public class TreeDrawerModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // navigation view
+            containerRegistry.RegisterForNavigation<ParentTreeView, ParentTreeViewModel>();
+        }
+    }
+}
---
+++ FamilyTree.Services.TreeDrawer.Interfaces/ITreeDrawer.cs
@@ -0,0 +1,12 @@
+﻿using FamilyTree.Modules.TreeDrawer.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FamilyTree.Services.TreeDrawer.Interfaces
+{
+    public interface ITreeDrawer
+    {
+        void Draw(ICollection<ITreeElement> treeElements);
+    }
+}
