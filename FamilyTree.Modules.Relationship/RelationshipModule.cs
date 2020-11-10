3a0f9a209fab435536c70ad588e174f0d459ca4f
tacsi97 <tacsi97@gmail.com>
2020-11-10 08:16:24 +0100

    Registration of classes

--- FamilyTree.Modules.Relationship/RelationshipModule.cs
+++ FamilyTree.Modules.Relationship/RelationshipModule.cs
@@ -1,4 +1,9 @@
-﻿using FamilyTree.Modules.Relationship.Views;
+﻿using FamilyTree.Core;
+using FamilyTree.Core.ApplicationCommands;
+using FamilyTree.Modules.Relationship.Repository;
+using FamilyTree.Modules.Relationship.ViewModels;
+using FamilyTree.Modules.Relationship.Views;
+using FamilyTree.Services.Repository.Interfaces;
 using Prism.Ioc;
 using Prism.Modularity;
 using Prism.Regions;
@@ -7,14 +12,29 @@ namespace FamilyTree.Modules.Relationship
 {
     public class RelationshipModule : IModule
     {
+        private readonly IRegionManager _regionManager;
+
+        public RelationshipModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
         public void OnInitialized(IContainerProvider containerProvider)
         {
-
+            //_regionManager.RegisterViewWithRegion(RegionNames.RelationshipRegion, typeof(SetRelationShipView));
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
         {
+            containerRegistry.RegisterSingleton<IAsyncRepository<Business.Relationship>, FakeRelationshipRepository>();
+            // Singleton
+            containerRegistry.RegisterSingleton<IUpload, UploadNewPersonCommand>();
 
+            containerRegistry.RegisterForNavigation<ListRelationshipView, ListRelationshipViewModel>();
+            containerRegistry.RegisterForNavigation<RelationshipFunctions, RelationshipFunctionsViewModel>();
+
+            containerRegistry.RegisterDialog<DeleteRelationshipView, DeleteRelationshipViewModel>(Core.DialogNames.DeleteRelationshipDialog);
+            containerRegistry.RegisterDialog<EditRelationshipView, EditRelationshipViewModel>(Core.DialogNames.EditRelationshipDialog);
         }
     }
 }
