6b12e831340a3ab4023521e000af4ba2eb5c55e2
tacsi97 <tacsi97@gmail.com>
2020-11-10 07:58:28 +0100

    Business objects for the TreeDrawer module

---
+++ FamilyTree.Business/Line.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyTree.Business
+{
+    public class Line
+    {
+        public double LeftCoordinate { get; set; }
+
+        public double TopCoordinate { get; set; }
+
+        public double RigthCoordinate { get; set; }
+
+        public double BottomCoordinate { get; set; }
+    }
+}
---
+++ FamilyTree.Business/Node.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FamilyTree.Business
+{
+    public class Node
+    {
+        public Person Person { get; set; }
+
+        public bool IsChecked { get; set; }
+
+        public double LeftCoordinate { get; set; }
+
+        public double TopCoordinate { get; set; }
+
+        public double RigthCoordinate { get => LeftCoordinate + Width; set { } }
+
+        public double BottomCoordinate { get => TopCoordinate + Height; set { } }
+
+        public double Width { get; set; }
+
+        public double Height { get; set; }
+
+        public Node LeftMostChild { get; set; }
+
+        public Node RightSibling { get; set; }
+
+        public Node(Person person)
+        {
+            Person = person;
+        }
+
+        public Node(Person person, double width, double height)
+        {
+            Person = person;
+            Width = width;
+            Height = height;
+        }
+    }
+}
