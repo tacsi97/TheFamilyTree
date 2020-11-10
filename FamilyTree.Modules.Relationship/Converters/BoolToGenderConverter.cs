e89f26a62be1a31f022f7177036c990a29f5d790
tacsi97 <tacsi97@gmail.com>
2020-11-10 08:11:03 +0100

    Converter added

---
+++ FamilyTree.Modules.Relationship/Converters/BoolToGenderConverter.cs
@@ -0,0 +1,25 @@
+﻿using FamilyTree.Business;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace FamilyTree.Modules.Relationship.Converters
+{
+    public class GenderToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((GenderType)parameter == (GenderType)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return parameter;
+
+            return Binding.DoNothing;
+        }
+    }
+}
