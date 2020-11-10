74f3f0bd9e2309fb961166091ac88ccc6a98e60c
tacsi97 <tacsi97@gmail.com>
2020-11-10 08:17:21 +0100

    Implemented adapters

---
+++ FamilyTree.Modules.TreeDrawer/Model/LineTreeElement.cs
@@ -0,0 +1,44 @@
+﻿using FamilyTree.Business;
+using FamilyTree.Modules.TreeDrawer.Adapters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyTree.Modules.TreeDrawer.Model
+{
+    public class LineTreeElement : TreeElementAdapter<Line>
+    {
+
+        #region Properties
+
+        public override double LeftCoordinate
+        {
+            get => TObject.LeftCoordinate;
+            set => TObject.LeftCoordinate = value;
+        }
+
+        public override double TopCoordinate
+        {
+            get => TObject.TopCoordinate;
+            set => TObject.TopCoordinate = value;
+        }
+
+        public override double RigthCoordinate
+        {
+            get => TObject.RigthCoordinate;
+            set => TObject.RigthCoordinate = value;
+        }
+
+        public override double BottomCoordinate
+        {
+            get => TObject.BottomCoordinate;
+            set => TObject.BottomCoordinate = value;
+        }
+
+        #endregion
+
+        public LineTreeElement(Line tObject) : base(tObject)
+        {
+        }
+    }
+}
---
+++ FamilyTree.Modules.TreeDrawer/Model/NodeTreeElement.cs
@@ -0,0 +1,43 @@
+﻿using FamilyTree.Business;
+using FamilyTree.Modules.TreeDrawer.Adapters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyTree.Modules.TreeDrawer.Model
+{
+    public class NodeTreeElement : TreeElementAdapter<Node>
+    {
+        #region Properties
+
+        public override double LeftCoordinate
+        {
+            get => TObject.LeftCoordinate;
+            set => TObject.LeftCoordinate = value;
+        }
+
+        public override double TopCoordinate
+        {
+            get => TObject.TopCoordinate;
+            set => TObject.TopCoordinate = value;
+        }
+
+        public override double RigthCoordinate
+        {
+            get => TObject.RigthCoordinate;
+            set => TObject.RigthCoordinate = value;
+        }
+
+        public override double BottomCoordinate
+        {
+            get => TObject.BottomCoordinate;
+            set => TObject.BottomCoordinate = value;
+        }
+
+        #endregion
+
+        public NodeTreeElement(Node node) : base(node)
+        {
+        }
+    }
+}
