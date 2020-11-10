b057c83e03865858151b5fb5edb3805778f2f748
tacsi97 <tacsi97@gmail.com>
2020-11-10 08:09:25 +0100

    New usercontrol by Prism

---
+++ FamilyTree.Modules.Person/Views/PersonRelationsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FamilyTree.Modules.Person.Views
+{
+    /// <summary>
+    /// Interaction logic for PersonRelationsView
+    /// </summary>
+    public partial class PersonRelationsView : UserControl
+    {
+        public PersonRelationsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
