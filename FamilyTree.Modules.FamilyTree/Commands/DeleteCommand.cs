8ff011dae062d7bfe43a7ff1dc3f00c516820675
tacsi97 <tacsi97@gmail.com>
2021-02-28 22:39:37 +0100

    delete command

---
+++ FamilyTree.Modules.FamilyTree/Commands/DeleteCommand.cs
@@ -0,0 +1,28 @@
+﻿using FamilyTree.Core.Commands;
+using FamilyTree.Core.Extensions;
+using FamilyTree.Modules.FamilyTree.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyTree.Modules.FamilyTree.Commands
+{
+    public class DeleteCommand : CommandBase<DeleteFamilyTreeViewViewModel>
+    {
+
+        public DeleteCommand(DeleteFamilyTreeViewViewModel ViewModel) : base(ViewModel)
+        {
+
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return ViewModel.CanExecuteDeleteCommand();
+        }
+
+        public override void Execute(object parameter)
+        {
+            ViewModel.ExecuteDeleteCommand().FireAndForgetAsync();
+        }
+    }
+}
