f2450e2a0a1eaebea94d14e66bc15a338c489005
tacsi97 <tacsi97@gmail.com>
2021-02-28 22:41:22 +0100

    new class

---
+++ FamilyTree.Modules.FamilyTree/ViewModels/DeleteFamilyTreeViewViewModel.cs
@@ -0,0 +1,77 @@
+﻿using FamilyTree.Core;
+using FamilyTree.Modules.FamilyTree.Commands;
+using FamilyTree.Modules.FamilyTree.Core;
+using FamilyTree.Services.Repository.Interfaces;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyTree.Modules.FamilyTree.ViewModels
+{
+    public class DeleteFamilyTreeViewViewModel : BindableBase, INavigationAware
+    {
+
+        #region Fields
+
+        private readonly IRegionManager _regionManager;
+        private readonly IAsyncRepository<Business.FamilyTree> _repository;
+
+        #endregion
+
+        #region Properties
+
+        private Business.FamilyTree _familyTree;
+        public Business.FamilyTree FamilyTree
+        {
+            get { return _familyTree; }
+            set { SetProperty(ref _familyTree, value); }
+        }
+
+        #endregion
+
+        #region Command
+
+        public DeleteCommand DeleteCommand { get; set; }
+
+        #endregion
+
+        public DeleteFamilyTreeViewViewModel(IRegionManager regionManager, IAsyncRepository<Business.FamilyTree> repository)
+        {
+            _regionManager = regionManager;
+            _repository = repository;
+
+            DeleteCommand = new DeleteCommand(this);
+        }
+
+        public async Task ExecuteDeleteCommand()
+        {
+            await _repository.DeleteAsync(FamilyTree.ID);
+
+            _regionManager.RequestNavigate(RegionNames.ContentRegion, "ListFamilyTreeView");
+        }
+
+        public bool CanExecuteDeleteCommand()
+        {
+            return true;
+        }
+
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+            return;
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            FamilyTree = navigationContext.Parameters.GetValue<Business.FamilyTree>(NavParamNames.Tree);
+        }
+    }
+}
---
+++ FamilyTree.Modules.FamilyTree/Views/DeleteFamilyTreeView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using FamilyTree.Core;
+using FamilyTree.Core.Attributes;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FamilyTree.Modules.FamilyTree.Views
+{
+    /// <summary>
+    /// Interaction logic for DeleteFamilyTreeView.xaml
+    /// </summary>
+
+    [DependentView(RegionNames.FunctionRegion, typeof(BackFamilyTreeView))]
+    public partial class DeleteFamilyTreeView : UserControl
+    {
+        public DeleteFamilyTreeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
