b4bc7df89f532ea80ab430dee080cae65c029402
tacsi97 <tacsi97@gmail.com>
2021-03-02 19:14:49 +0100

    navigáció paraméterek

---
+++ FamilyTree.Modules.Relationship/Core/NavParamNames.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyTree.Modules.Relationship.Core
+{
+    public class NavParamNames
+    {
+        public static string Relationship = "Relationship";
+    }
+}
