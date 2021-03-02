2acd4bb3d66efac53433f150920dde3ea0179a03
tacsi97 <tacsi97@gmail.com>
2021-03-02 19:58:39 +0100

    dependent view

--- FamilyTree.Modules.Relationship/Views/EditRelationshipView.xaml.cs
+++ FamilyTree.Modules.Relationship/Views/EditRelationshipView.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System.Windows.Controls;
+﻿using FamilyTree.Core;
+using FamilyTree.Core.Attributes;
+using System.Windows.Controls;
 
 namespace FamilyTree.Modules.Relationship.Views
 {
     /// <summary>
     /// Interaction logic for EditRelationshipView
     /// </summary>
+    
+    [DependentView(RegionNames.ContentRegion, typeof(BackRelationshipView))]
     public partial class EditRelationshipView : UserControl
     {
         public EditRelationshipView()
