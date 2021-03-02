cf697ff5e9b46b8e3dc52d23e1b50c0d697768b2
tacsi97 <tacsi97@gmail.com>
2021-03-02 19:28:39 +0100

    létrehozás és regionmanager injektálás

---
+++ FamilyTree.Modules.Relationship/ViewModels/BackRelationshipViewViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FamilyTree.Modules.Relationship.ViewModels
+{
+    public class BackRelationshipViewViewModel : BindableBase
+    {
+        #region Fields
+
+        private readonly IRegionManager _regionManager;
+
+        #endregion
+
+        #region Commands
+
+
+
+        #endregion
+
+        public BackRelationshipViewViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+    }
+}
---
+++ FamilyTree.Modules.Relationship/Views/BackRelationshipView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FamilyTree.Modules.Relationship.Views
+{
+    /// <summary>
+    /// Interaction logic for BackRelationshipView
+    /// </summary>
+    public partial class BackRelationshipView : UserControl
+    {
+        public BackRelationshipView()
+        {
+            InitializeComponent();
+        }
+    }
+}
