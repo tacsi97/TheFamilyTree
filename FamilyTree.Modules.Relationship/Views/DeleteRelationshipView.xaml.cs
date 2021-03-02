4abfee7daf808f5c7b6b2101b79d8c8a30bfff62
tacsi97 <tacsi97@gmail.com>
2021-03-02 19:55:25 +0100

    dependent view

--- FamilyTree.Modules.Relationship/Views/DeleteRelationshipView.xaml.cs
+++ FamilyTree.Modules.Relationship/Views/DeleteRelationshipView.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System.Windows.Controls;
+﻿using FamilyTree.Core;
+using FamilyTree.Core.Attributes;
+using System.Windows.Controls;
 
 namespace FamilyTree.Modules.Relationship.Views
 {
     /// <summary>
     /// Interaction logic for DeleteRelationshipView
     /// </summary>
+    
+    [DependentView(RegionNames.ContentRegion, typeof(BackRelationshipView))]
     public partial class DeleteRelationshipView : UserControl
     {
         public DeleteRelationshipView()
