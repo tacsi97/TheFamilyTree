24b708c34502fa1b501328c1de8817edc098b8ad
tacsi97 <tacsi97@gmail.com>
2021-03-02 09:02:37 +0100

    delete person view módosítás

--- FamilyTree.Modules.Person/Views/DeletePersonView.xaml.cs
+++ FamilyTree.Modules.Person/Views/DeletePersonView.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System.Windows.Controls;
+﻿using FamilyTree.Core;
+using FamilyTree.Core.Attributes;
+using System.Windows.Controls;
 
 namespace FamilyTree.Modules.Person.Views
 {
     /// <summary>
     /// Interaction logic for DeletePersonView
     /// </summary>
+
+    [DependentView(RegionNames.FunctionRegion, typeof(BackPersonView))]
     public partial class DeletePersonView : UserControl
     {
         public DeletePersonView()
