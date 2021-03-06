ea2540bb2b542e2df2ffcbbf717cd52ab9c02281
toth.laszlo <toth.laszlo@promera.hu>
2021-03-06 20:15:26 +0100

    leftmost értékkel működik

--- FamilyTree.Services.TreeTravelsal/ParentTraverse.cs
+++ FamilyTree.Services.TreeTravelsal/ParentTraverse.cs
@@ -7,6 +7,12 @@ namespace FamilyTree.Services.TreeTravelsal
 {
     public class ParentTraverse : ITreeTraversal<Business.Node>
     {
+        #region Fields
+
+        private double _leftmostValue = 0d;
+
+        #endregion
+
         #region Properties
 
         public ICollection<Business.Node> Nodes { get; set; }
@@ -39,7 +45,42 @@ namespace FamilyTree.Services.TreeTravelsal
             PostOrder(node.Father);
 
             Visit(node);
+
+            if (node.Mother.Person == null && node.Father.Person == null)
+            {
+                node.LeftCoordinate = _leftmostValue;
+                _leftmostValue += node.Width + 25;
+            }
+            else if (node.Mother.Person == null)
+                node.LeftCoordinate = node.Father.LeftCoordinate;
+            else if (node.Father.Person == null)
+                node.LeftCoordinate = node.Mother.LeftCoordinate;
+            else
+                node.LeftCoordinate = (node.Mother.LeftCoordinate + node.Father.LeftCoordinate) * 0.5;
         }
+
+        //public void PostOrder(Business.Node node)
+        //{
+        //    // TODO: Vagy itt kötögetem össze az embereket, vagy egy másik fa bejárásban
+        //    // Másik fa bejárás:
+        //    //      2 service kellene, egyik csak összeköti az embereket, a másik csak kiírja
+        //    //      kétszer futna le a fa bejárás
+        //    if (node == null || node.Person == null) return;
+
+        //    node.Mother = new Business.Node(node.Person.Mother);
+        //    node.Mother.LeftMostChild = node;
+        //    node.Mother.TopCoordinate = node.TopCoordinate - (node.Height + 25);
+        //    node.Mother.LeftCoordinate = node.LeftCoordinate - (node.Width + 25) * 0.5;
+        //    PostOrder(node.Mother);
+
+        //    node.Father = new Business.Node(node.Person.Father);
+        //    node.Father.LeftMostChild = node;
+        //    node.Father.TopCoordinate = node.TopCoordinate - (node.Height + 25);
+        //    node.Father.LeftCoordinate = node.LeftCoordinate + (node.Width + 25) * 0.5;
+        //    PostOrder(node.Father);
+
+        //    Visit(node);
+        //}
 
         public void PreOrder(Business.Node node)
         {
