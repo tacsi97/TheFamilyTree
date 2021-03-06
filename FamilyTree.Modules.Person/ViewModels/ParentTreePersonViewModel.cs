c076c82dee2d73441c9446f5f0c0c5551ced3982
toth.laszlo <toth.laszlo@promera.hu>
2021-03-06 12:37:54 +0100

    INavigationAware implementálás

--- FamilyTree.Modules.Person/ViewModels/ParentTreePersonViewModel.cs
+++ FamilyTree.Modules.Person/ViewModels/ParentTreePersonViewModel.cs
@@ -1,13 +1,15 @@
-﻿using FamilyTree.Services.Repository.Interfaces;
+﻿using FamilyTree.Modules.Person.Core;
+using FamilyTree.Services.Repository.Interfaces;
 using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Regions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace FamilyTree.Modules.Person.ViewModels
 {
-    public class ParentTreePersonViewModel : BindableBase
+    public class ParentTreePersonViewModel : BindableBase, INavigationAware
     {
         #region Fields
 
@@ -39,5 +41,19 @@ namespace FamilyTree.Modules.Person.ViewModels
 
         }
 
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            SelectedPerson = navigationContext.Parameters.GetValue<Business.Person>(NavParamNames.Person);
+        }
+
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+            return;
+        }
     }
 }
