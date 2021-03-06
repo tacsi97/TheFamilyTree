25df146b5a4d72e7cf0a967e6609dba3ceb0d3f1
toth.laszlo <toth.laszlo@promera.hu>
2021-03-06 12:41:40 +0100

    Function view beszúrás

--- FamilyTree.Modules.Person/Views/ParentTreePersonView.xaml.cs
+++ FamilyTree.Modules.Person/Views/ParentTreePersonView.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System.Windows.Controls;
+﻿using FamilyTree.Core;
+using FamilyTree.Core.Attributes;
+using System.Windows.Controls;
 
 namespace FamilyTree.Modules.Person.Views
 {
     /// <summary>
     /// Interaction logic for ParentTreePersonView
     /// </summary>
+
+    [DependentView(RegionNames.FunctionRegion, typeof(PersonFunctions))]
     public partial class ParentTreePersonView : UserControl
     {
         public ParentTreePersonView()
