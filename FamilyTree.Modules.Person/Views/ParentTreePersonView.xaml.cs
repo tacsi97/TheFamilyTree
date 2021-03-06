90f71a53b2b70aca03632a8ca61a29f2fc641db7
toth.laszlo <toth.laszlo@promera.hu>
2021-03-06 12:29:39 +0100

    new usercontrol

---
+++ FamilyTree.Modules.Person/Views/ParentTreePersonView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FamilyTree.Modules.Person.Views
+{
+    /// <summary>
+    /// Interaction logic for ParentTreePersonView
+    /// </summary>
+    public partial class ParentTreePersonView : UserControl
+    {
+        public ParentTreePersonView()
+        {
+            InitializeComponent();
+        }
+    }
+}
