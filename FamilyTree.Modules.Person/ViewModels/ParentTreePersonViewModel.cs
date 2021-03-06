f614a9410b912c94168f66f64740fd450793db37
toth.laszlo <toth.laszlo@promera.hu>
2021-03-06 12:36:31 +0100

    draw command

--- FamilyTree.Modules.Person/ViewModels/ParentTreePersonViewModel.cs
+++ FamilyTree.Modules.Person/ViewModels/ParentTreePersonViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Commands;
+﻿using FamilyTree.Services.Repository.Interfaces;
+using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
@@ -8,9 +9,35 @@ namespace FamilyTree.Modules.Person.ViewModels
 {
     public class ParentTreePersonViewModel : BindableBase
     {
-        public ParentTreePersonViewModel()
+        #region Fields
+
+        private readonly IAsyncRepository<Business.Person> _repository;
+
+        #endregion
+
+        #region Properties
+
+        public Business.Person SelectedPerson { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        private DelegateCommand _drawCommand;
+        public DelegateCommand DrawCommand =>
+            _drawCommand ?? (_drawCommand = new DelegateCommand(ExecuteDrawCommand));
+
+        #endregion
+
+        public ParentTreePersonViewModel(IAsyncRepository<Business.Person> repository)
+        {
+            _repository = repository;
+        }
+
+        public void ExecuteDrawCommand()
         {
 
         }
+
     }
 }
