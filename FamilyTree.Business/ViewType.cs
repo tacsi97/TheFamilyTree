109fc268bf524586f160f8283679037695671f9b
toth.laszlo <toth.laszlo@promera.hu>
2021-03-16 13:30:28 +0100

    .

---
+++ FamilyTree.Business/ViewType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyTree.Business
+{
+    public enum ViewType
+    {
+        ListView,
+        ParentView,
+        ChildrenView
+    }
+}
