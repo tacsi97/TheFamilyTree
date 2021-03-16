a832f9e7f76b8bc59efd72f394fbc235449c61e4
toth.laszlo <toth.laszlo@promera.hu>
2021-03-16 13:29:36 +0100

    pubsubevent

---
+++ FamilyTree/FamilyTree.Core/PubSubEvents/SelectedViewTypeChangedEvent.cs
@@ -0,0 +1,12 @@
+﻿using FamilyTree.Business;
+using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyTree.Core.PubSubEvents
+{
+    public class SelectedViewTypeChangedEvent : PubSubEvent<ViewType>
+    {
+    }
+}
