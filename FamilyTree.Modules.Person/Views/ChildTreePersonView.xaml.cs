607a21dc517e09593a347d2e840fd477d5d64fdd
toth.laszlo <toth.laszlo@promera.hu>
2021-03-16 13:32:00 +0100

    .

--- FamilyTree.Modules.Person/Views/ChildTreePersonView.xaml.cs
+++ FamilyTree.Modules.Person/Views/ChildTreePersonView.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System.Windows.Controls;
+﻿using FamilyTree.Core;
+using FamilyTree.Core.Attributes;
+using System.Windows.Controls;
 
 namespace FamilyTree.Modules.Person.Views
 {
     /// <summary>
     /// Interaction logic for ChildTreePersonView
     /// </summary>
+    /// 
+    [DependentView(RegionNames.FunctionRegion, typeof(PersonFunctions))]
     public partial class ChildTreePersonView : UserControl
     {
         public ChildTreePersonView()
