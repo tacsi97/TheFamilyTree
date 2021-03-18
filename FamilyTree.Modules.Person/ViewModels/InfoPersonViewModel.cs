2e6df3f60940ff4269e210eb7a552ac24277e68d
toth.laszlo <toth.laszlo@promera.hu>
2021-03-18 15:52:59 +0100

    info nézet

--- FamilyTree.Modules.Person/ViewModels/InfoPersonViewModel.cs
+++ FamilyTree.Modules.Person/ViewModels/InfoPersonViewModel.cs
@@ -1,33 +1,184 @@
-﻿using FamilyTree.Core.Mvvm;
+﻿using FamilyTree.Business;
+using FamilyTree.Core;
+using FamilyTree.Core.Commands;
+using FamilyTree.Core.Mvvm;
 using FamilyTree.Modules.Person.Core;
 using FamilyTree.Services.Repository.Interfaces;
+using Microsoft.Win32;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Regions;
 using Prism.Services.Dialogs;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace FamilyTree.Modules.Person.ViewModels
 {
     public class InfoPersonViewModel : BindableBase, INavigationAware
     {
-        private Business.Person _person;
-        public Business.Person Person
+        #region Fields
+
+        private readonly IAsyncRepository<Business.Person> _repository;
+        private readonly IRegionManager _regionManager;
+
+        #endregion
+
+        #region Properties
+
+        private int _id;
+        public int ID
         {
-            get { return _person; }
-            set { SetProperty(ref _person, value); }
+            get { return _id; }
+            set { SetProperty(ref _id, value); }
         }
 
-        public InfoPersonViewModel()
+        private string _firstName;
+        public string FirstName
         {
+            get { return _firstName; }
+            set
+            {
+                SetProperty(ref _firstName, value);
+                AsyncCommand.RaiseCanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
 
+        private string _lastName;
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                SetProperty(ref _lastName, value);
+                AsyncCommand.RaiseCanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+
+        private DateTime _dateOfBirth;
+        public DateTime DateOfBirth
+        {
+            get { return _dateOfBirth; }
+            set
+            {
+                SetProperty(ref _dateOfBirth, value);
+                AsyncCommand.RaiseCanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+
+        private DateTime _dateOfDeath;
+        public DateTime DateOfDeath
+        {
+            get { return _dateOfDeath; }
+            set
+            {
+                SetProperty(ref _dateOfDeath, value);
+                AsyncCommand.RaiseCanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+
+        private GenderType _gender;
+        public GenderType Gender
+        {
+            get { return _gender; }
+            set
+            {
+                SetProperty(ref _gender, value);
+                AsyncCommand.RaiseCanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+
+        private BitmapImage _image;
+        public BitmapImage Image
+        {
+            get { return _image; }
+            set { SetProperty(ref _image, value); }
+        }
+
+        public string Title => "Személy létrehozása";
+
+        private DelegateCommand _selectPictureCommand;
+        public DelegateCommand SelectPictureCommand =>
+            _selectPictureCommand ?? (_selectPictureCommand = new DelegateCommand(ExecuteSelectPictureCommand));
+
+        void ExecuteSelectPictureCommand()
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+                        "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+                        "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+                Image = new BitmapImage(new Uri(op.FileName));
+        }
+
+        public ObservableCollection<Business.Person> People { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        private AsyncCommand _asyncCommand;
+        public AsyncCommand AsyncCommand
+        {
+            get { return _asyncCommand; }
+            set { SetProperty(ref _asyncCommand, value); }
+        }
+
+        #endregion
+
+        public InfoPersonViewModel(IAsyncRepository<Business.Person> repository, IRegionManager regionManager)
+        {
+            _repository = repository;
+            _regionManager = regionManager;
+
+            AsyncCommand = new AsyncCommand(ExecuteSubmit, CanExecuteSubmit);
+        }
+
+        public async Task ExecuteSubmit()
+        {
+            try
+            {
+                var person = new Business.Person()
+                {
+                    ID = ID,
+                    FirstName = FirstName,
+                    LastName = LastName,
+                    DateOfBirth = DateOfBirth,
+                    DateOfDeath = DateOfDeath,
+                    Gender = Gender
+                };
+
+                await _repository.ModifyAsync(person);
+
+                var navParams = new NavigationParameters();
+                navParams.Add(NavParamNames.Person, person);
+
+                _regionManager.RequestNavigate(RegionNames.ContentRegion, "ListPersonView", navParams);
+            }
+            catch (Exception e)
+            {
+                // TODO: exception handling
+            }
+        }
+
+        public bool CanExecuteSubmit()
+        {
+            return true;
         }
 
         public void OnNavigatedTo(NavigationContext navigationContext)
         {
-            Person = navigationContext.Parameters.GetValue<Business.Person>(NavParamNames.Person);
+            var person = navigationContext.Parameters.GetValue<Business.Person>(NavParamNames.Person);
+
+            ID = person.ID;
+            FirstName = person.FirstName;
+            LastName = person.LastName;
+            DateOfBirth = person.DateOfBirth;
+            DateOfDeath = person.DateOfDeath;
+            Gender = person.Gender;
         }
 
         public bool IsNavigationTarget(NavigationContext navigationContext)
