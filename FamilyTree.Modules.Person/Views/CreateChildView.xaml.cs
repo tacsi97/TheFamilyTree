d03b6f8fbbd6c4f4848081825ebd400dde57c77a
toth.laszlo <toth.laszlo@promera.hu>
2021-03-20 15:35:55 +0100

    dependent view

--- FamilyTree.Modules.Person/Views/CreateChildView.xaml.cs
+++ FamilyTree.Modules.Person/Views/CreateChildView.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System.Windows.Controls;
+﻿using FamilyTree.Core;
+using FamilyTree.Core.Attributes;
+using System.Windows.Controls;
 
 namespace FamilyTree.Modules.Person.Views
 {
     /// <summary>
     /// Interaction logic for CreateChildView
     /// </summary>
+    [DependentView(RegionNames.FunctionRegion, typeof(BackPersonView))]
     public partial class CreateChildView : UserControl
     {
         public CreateChildView()
