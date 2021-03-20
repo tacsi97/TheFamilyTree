e8c7c7de7dd15a40988003c468c1d769dba88908
toth.laszlo <toth.laszlo@promera.hu>
2021-03-20 15:06:29 +0100

    új control létrehozása meglévő alapján, majd tisztogatás

---
+++ FamilyTree.Modules.Person/ViewModels/CreateFatherViewModel.cs
@@ -0,0 +1,98 @@
+﻿using FamilyTree.Business;
+using FamilyTree.Core;
+using FamilyTree.Core.Commands;
+using FamilyTree.Core.PubSubEvents;
+using FamilyTree.Modules.Person.Commands;
+using FamilyTree.Modules.Person.Core;
+using FamilyTree.Services.Repository.Interfaces;
+using Prism.Commands;
+using Prism.Events;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyTree.Modules.Person.ViewModels
+{
+    public class CreateFatherViewModel : BindableBase, INavigationAware
+    {
+        private readonly IAsyncRepository<Business.Person> _repository;
+        private readonly IRegionManager _regionManager;
+
+        #region Commands
+
+        private AsyncCommand _asyncCommand;
+        public AsyncCommand AsyncCommand
+        {
+            get { return _asyncCommand; }
+            set { SetProperty(ref _asyncCommand, value); }
+        }
+
+        #endregion
+
+        #region Properties
+        private Business.Person _selectedPerson;
+        public Business.Person SelectedPerson
+        {
+            get { return _selectedPerson; }
+            set { SetProperty(ref _selectedPerson, value); }
+        }
+
+        private Business.Person _newPerson = new Business.Person();
+        public Business.Person NewPerson
+        {
+            get { return _newPerson; }
+            set { SetProperty(ref _newPerson, value); }
+        }
+
+        public string Title => "Személy létrehozása";
+
+        #endregion
+
+        public CreateFatherViewModel(IAsyncRepository<Business.Person> repository, IRegionManager regionManager)
+        {
+            _repository = repository;
+            _regionManager = regionManager;
+
+            NewPerson = new Business.Person();
+
+            AsyncCommand = new AsyncCommand(Submit, CanExecuteSubmit);
+        }
+
+        public async Task Submit()
+        {
+            try
+            {
+                await _repository.CreateAsync(NewPerson);
+
+                _regionManager.RequestNavigate(RegionNames.ContentRegion, "ListPersonView");
+            }
+            catch (Exception e)
+            {
+                // TODO: exception handling
+            }
+        }
+
+        public bool CanExecuteSubmit()
+        {
+            return true;
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            SelectedPerson = navigationContext.Parameters.GetValue<Business.Person>(NavParamNames.Person);
+        }
+
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+            return;
+        }
+    }
+}
