c0c45babcb98f2327dad421c3368bd2050048576
toth.laszlo <toth.laszlo@promera.hu>
2021-03-20 15:35:05 +0100

    új control

---
+++ FamilyTree.Modules.Person/Views/CreateChildView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FamilyTree.Modules.Person.Views
+{
+    /// <summary>
+    /// Interaction logic for CreateChildView
+    /// </summary>
+    public partial class CreateChildView : UserControl
+    {
+        public CreateChildView()
+        {
+            InitializeComponent();
+        }
+    }
+}
