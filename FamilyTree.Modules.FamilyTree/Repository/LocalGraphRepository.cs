68135577cb159471c81db9d6ad7310389c4bad93
toth.laszlo <toth.laszlo@promera.hu>
2021-03-22 16:05:06 +0100

    ...

---
+++ FamilyTree.Modules.FamilyTree/Repository/LocalGraphRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyTree.Modules.FamilyTree.Repository
+{
+    class LocalGraphRepository
+    {
+    }
+}
---
+++ FamilyTree/FamilyTree.Core/DatabaseConnector/Neo4JConnector.cs
@@ -0,0 +1,22 @@
+﻿using Neo4j.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyTree.Core.DatabaseConnector
+{
+    public abstract class Neo4JConnector : IDisposable
+    {
+        private readonly IDriver _driver;
+
+        public Neo4JConnector(string uri, string user, string password)
+        {
+            _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+        }
+
+        public void Dispose()
+        {
+            _driver?.Dispose();
+        }
+    }
+}
