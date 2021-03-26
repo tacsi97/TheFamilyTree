634e0459ae775f5ab94d832bd284629b57f6ca62
toth.laszlo <toth.laszlo@promera.hu>
2021-03-26 14:19:10 +0100

    string id

--- FamilyTree.Business/BusinessBase.cs
+++ FamilyTree.Business/BusinessBase.cs
@@ -9,9 +9,9 @@ namespace FamilyTree.Business
     public class BusinessBase : BindableBase
     {
         // TODO: id-t átalakítani string-re, mivel a gráf adatbázis azt fogja használni
-        private int _id;
+        private string _id;
         [JsonProperty("ID")]
-        public int ID
+        public string ID
         {
             get { return _id; }
             set { SetProperty(ref _id, value); }
